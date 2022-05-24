a56cd141e670053a7206448f9ca3226d18cf0a0a
cDev-Creator <mccoyng170@gmail.com>
2022-05-24 14:59:24 -0400

    initial commit

---
+++ addStudent.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+/* $priority = filter_input(INPUT_POST, "priority", FILTER_VALIDATE_INT);
+ */
+
+$host = "localhost";
+$dbname = "students";
+$username = "root";
+$password = "";
+        
+$conn = mysqli_connect(hostname: $host,
+                       username: $username,
+                       password: $password,
+                       database: $dbname);
+        
+if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+}           
+        
+$sql = "INSERT INTO message (firstName, lastName)
+        VALUES (?, ?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+ 
+    die(mysqli_error($conn));
+}
+
+mysqli_stmt_bind_param($stmt, "ss",
+                       $firstName,
+                       $lastName);
+
+mysqli_stmt_execute($stmt);
+
+echo "Record saved.";
+?>
