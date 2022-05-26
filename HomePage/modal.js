6ea23da865976c2fc87646fc4af3214becd72783
cDev-Creator <mccoyng170@gmail.com>
2022-05-25 22:21:15 -0400

    class sign-in modal

---
+++ HomePage/modal.js
@@ -0,0 +1,16 @@
+
+document.getElementById('orderLunchBtn').addEventListener('click', 
+function() {
+    document.querySelector('.modal-bg').style.display = 'flex';
+
+})
+
+/* document.getElementById('submitBtn').addEventListener('click',
+function() {
+
+}) */
+
+document.getElementById('closeBtn').addEventListener('click', 
+function() {
+    document.querySelector('.modal-bg').style.display = 'none';
+});
