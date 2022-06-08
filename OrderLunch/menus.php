bc4b33ac4eb5b6014e94f9bb64f1221a1fb486a7
cDev-Creator <mccoyng170@gmail.com>
2022-06-08 16:38:43 -0400

    day of the week incriments

--- OrderLunch/menus.php
+++ OrderLunch/menus.php
@@ -1,9 +1,40 @@
 <?php
-echo "Date: ";
-echo date("m-d-Y");
-echo "</br>";
-$jd=gregoriantojd(1,27,2023);
-echo "Day of the week : " .jddayofweek($jd,1);
-echo "</br>";
 
+// Return date/time info of a timestamp; then format the output
+$mydate=getdate(date("U"));
+echo "Day of the week : ";
+$weekday = "$mydate[weekday]";
+echo $weekday;
+echo"</br>";
+echo "Date: ";
+echo "$mydate[month] $mydate[mday], $mydate[year]";
+
+if($weekday == "Friday") {
+    echo 'Rat baby works';
+}
+
+echo"</br>";
+echo "Restaraunt:";
+
+switch ($weekday) {
+case "Monday":
+    echo " 54 Pizza";
+    break;
+case "Tuesday":
+    echo " Chick-fil-A";
+    break;
+case "Wednesday":
+    echo " Ritzy's";
+    break;
+case "Thursday":
+    echo " Arby's";
+    break;
+case "Friday":
+    echo " Great Harvest";
+    break;
+default:
+    echo "School is not in session!";
+}
+
+echo "</br>"
 ?>
