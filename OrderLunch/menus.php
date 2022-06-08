92a7ae97d85b3ffe79bc80cebc33bbf0e0f9aff0
cDev-Creator <mccoyng170@gmail.com>
2022-06-08 18:39:44 -0400

    menu items synced to day of the week

--- OrderLunch/menus.php
+++ OrderLunch/menus.php
@@ -1,40 +1,127 @@
 <?php
-
-// Return date/time info of a timestamp; then format the output
+define('TIMEZONE', 'America/Chicago');
+date_default_timezone_set(TIMEZONE);
 $mydate=getdate(date("U"));
+echo "Date: ";
+echo "$mydate[month] $mydate[mday], $mydate[year]";
+echo"</br>";
 echo "Day of the week : ";
 $weekday = "$mydate[weekday]";
 echo $weekday;
 echo"</br>";
-echo "Date: ";
-echo "$mydate[month] $mydate[mday], $mydate[year]";
-
-if($weekday == "Friday") {
-    echo 'Rat baby works';
-}
-
 echo"</br>";
-echo "Restaraunt:";
 
+//CLEAN UP REPETITIVE CODE
+echo "Menu:";
 switch ($weekday) {
 case "Monday":
-    echo " 54 Pizza";
+    ?>
+    <?php
+    if($weekday == "Monday") {
+    $mysqli = new mysqli('localhost','root','','menus');
+    $resultSet = $mysqli->query("SELECT item, price FROM 54pizza");
+    ?>
+
+    <select name="menuItems">
+    <?php
+    while($rows = $resultSet->fetch_assoc())
+    {
+        $item = $rows['item'];
+        $price = $rows['price'];
+        echo "<option id='menuItems' value='$item'> $item</option>";
+    }
+    ?>
+    </select>
+    <?php
+    }
     break;
+
 case "Tuesday":
-    echo " Chick-fil-A";
+    ?>
+    <?php
+    if($weekday == "Tuesday") {
+    $mysqli = new mysqli('localhost','root','','menus');
+    $resultSet = $mysqli->query("SELECT item, price FROM arbys");
+    ?>
+
+    <select name="menuItems">
+    <?php
+    while($rows = $resultSet->fetch_assoc())
+    {
+        $item = $rows['item'];
+        $price = $rows['price'];
+        echo "<option id='menuItems' value='$item'> $item</option>";
+    }
+    ?>
+    </select>
+    <?php
+    }
     break;
 case "Wednesday":
-    echo " Ritzy's";
+    ?>
+    <?php
+    if($weekday == "Wednesday") {
+    $mysqli = new mysqli('localhost','root','','menus');
+    $resultSet = $mysqli->query("SELECT item, price FROM ritzys");
+    ?>
+
+    <select name="menuItems">
+    <?php
+    while($rows = $resultSet->fetch_assoc())
+    {
+        $item = $rows['item'];
+        $price = $rows['price'];
+        echo "<option id='menuItems' value='$item'> $item</option>";
+    }
+
+    ?>
+    </select>
+    <?php
+    }
     break;
 case "Thursday":
-    echo " Arby's";
+    ?>
+    <?php
+    if($weekday == "Thursday") {
+    $mysqli = new mysqli('localhost','root','','menus');
+    $resultSet = $mysqli->query("SELECT item, price FROM chickfila");
+    ?>
+
+    <select name="menuItems">
+    <?php
+    while($rows = $resultSet->fetch_assoc())
+    {
+        $item = $rows['item'];
+        $price = $rows['price'];
+        echo "<option id='menuItems' value='$item'> $item</option>";
+    }
+    ?>
+    </select>
+    <?php
+    }
     break;
 case "Friday":
-    echo " Great Harvest";
+    ?>
+    <?php
+    if($weekday == "Friday") {
+    $mysqli = new mysqli('localhost','root','','menus');
+    $resultSet = $mysqli->query("SELECT item, price FROM greatharvest");
+    ?>
+
+    <select name="menuItems">
+    <?php
+    while($rows = $resultSet->fetch_assoc())
+    {
+        $item = $rows['item'];
+        $price = $rows['price'];
+        echo "<option id='menuItems' value='$item'> $item</option>";
+    }
+
+    ?>
+    </select>
+    <?php
+    }
     break;
 default:
     echo "School is not in session!";
 }
-
-echo "</br>"
-?>
