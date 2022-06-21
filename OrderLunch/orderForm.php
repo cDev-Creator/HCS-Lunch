ef2488b9375bb2b852574cb8f525f421c57239a1
cDev-Creator <mccoyng170@gmail.com>
2022-06-21 17:17:38 -0400

    basic markup of office order summary chart

--- OrderLunch/orderForm.php
+++ OrderLunch/orderForm.php
@@ -1,3 +1,100 @@
-<?php
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<table id="table" width="900px" border="1" cellspacing="1" cellpadding="1">
+    
+    <tr>
+        <td>menu item 1</td>
+        <td>menu item 2</td>
+        <td>menu item 3</td>
+        <td>menu item 4</td>
+        <td>menu item 5</td>
+    </tr>
+
+    <tr>
+        <td align="center" colspan="5">1st Shift</td>
+    </tr>
+    <tr>
+        <td>Office Stadff</td>  
+    </tr>
+    <tr>
+        <td>2 Day Preschool</td>
+    </tr>
+
+    <tr>
+        <td>3 Day Preschool</td>
+    </tr>
+
+
+    <tr>
+        <td>1st Grade</td>
+    </tr>
+
+    <tr>
+        <td>2nd Grade</td>
+    </tr>
+
+    <tr>
+        <td>3rd Grade</td>
+    </tr>
+
+    <tr>
+        <td>5th Grade</td>
+    </tr>
+
+    <tr>
+        <td>1st Shift Total</td>
+    </tr>
+
+    <tr>
+        <td align="center" colspan="5">2nd Shift</td>
+    </tr>
+
+    <tr>
+        <td>6th Grade</td>
+    </tr>
+
+    <tr>
+        <td>7th Grade</td>
+    </tr>
+
+    <tr>
+        <td>8th Grade</td>
+    </tr>
+
+    <tr>
+        <td>2nd Shift Total</td>
+    </tr>
+
+    <tr>
+        <td>Total Ordered</td>
+    </tr>
+
+    <tr>
+        <td>Item Price</td>
+    </tr>
+
+    <tr>
+        <td>Total Cost</td>
+    </tr>
+
+</table>
+
+ 
+
+
+</body>
+</html>
+
+<script>
+
+</script>
+
