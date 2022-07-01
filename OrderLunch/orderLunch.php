53d8665403af20565406e364fb9b728be26aa656
cDev-Creator <mccoyng170@gmail.com>
2022-07-01 17:00:42 -0400

    ajax to correctly update orders table database for each order

--- OrderLunch/orderLunch.php
+++ OrderLunch/orderLunch.php
@@ -1,10 +1,12 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 <?php
 include('menus.php')
 ?>
 
 <?php
-$staffID = $_GET['staffID'];
-$grade = $_GET['grade'];
+$staffID = $_POST['staffID'];
+$grade = $_POST['grade'];
 
 echo "<br/>";
 echo "<br/>";
@@ -22,21 +24,24 @@ echo "All Students:";
 ?>
 
 <?php
-$grade = $_GET['grade'];
+$grade = $_POST['grade'];
 ?>
 
 <!-- STUDENTS FROM OTHER CLASSES  -->
 <?php
 $mysqli = new mysqli('localhost','root','','students');
-$grade = $_GET['grade'];
+$grade = $_POST['grade'];
 
 /* $result = $mysqli->query("SELECT * FROM names WHERE grade='$grade' ORDER BY grade, firstName ASC"); */
 $result = $mysqli->query("SELECT * FROM names ORDER BY grade, firstName ASC"); 
 ?>
 
-<form method="POST" onsubmit="populateTable(); return false;"> 
+<form name="lunch-form" id="lunch-form" method="POST" onsubmit="populateTable(); return false;"> 
+
 <select name="students" id="allStudents">
 <option value='' selected='selected'>select</option>;
+
+
 <?php
 while($rows = $result->fetch_assoc())
 {
@@ -48,12 +53,8 @@ while($rows = $result->fetch_assoc())
 ?>
 
 </select>
-
-<button type="submit" name="submit" id="submitBtn">Submit</button>
-</form>
-
 <?php
-$grade = $_GET['grade'];
+$grade = $_POST['grade'];
 echo  $grade. " Students:";
 ?>
 
@@ -61,11 +62,10 @@ echo  $grade. " Students:";
 <?php
 $mysqli = new mysqli('localhost','root','','students');
 $resultSet = $mysqli->query("SELECT firstName, lastName, grade FROM names");
-$grade = $_GET['grade'];
+$grade = $_POST['grade'];
 $result = $mysqli->query("SELECT * FROM names WHERE grade='$grade' order by firstName ASC ");
 
 ?>
-
 <select name="students" id="students">
 <option value='' selected='selected'>select</option>;
 <?php
@@ -79,5 +79,26 @@ while($rows = $result->fetch_assoc())
 } 
 ?>
 
+</select>
+<button type="submit" name="submit" id="submitBtn">Submit Test</button>
+</form>
 
-
+<!--AJAX-->
+<p id="message"></p>  
+ <script>
+    $(document).ready(function() {
+        $("#lunch-form").submit(function(e) {
+            e.preventDefault();
+            $.ajax( {
+                url: "ordersDB.php",
+                method: "post",
+                data: $("form").serialize(),
+                dataType: "text",
+                success: function(strMessage) {
+                    $("#message").text(strMessage);
+                    $("#lunch-form")[0].reset();
+                }
+            });
+        });
+    });
+</script>
