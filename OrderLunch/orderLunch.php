c2f35d893d02b745e4769e422919a62b23360f03
cDev-Creator <mccoyng170@gmail.com>
2022-07-20 16:13:17 -0400

    session storage for grade and staffID

--- OrderLunch/orderLunch.php
+++ OrderLunch/orderLunch.php
@@ -1,8 +1,13 @@
+<?php
+session_start();
+?>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 <?php
-$staffID = $_POST['staffID'];
-$grade = $_POST['grade'];
+$staffID = $_SESSION["staffID"] = $_POST['staffID'] ?? $_SESSION["staffID"];
+$grades = $_SESSION["grade"] = $_POST['grade'] ?? $_SESSION["grade"];
+
 
 $mydate=getdate(date("U"));
 echo "Date: ";
@@ -10,23 +15,21 @@ echo "$mydate[month] $mydate[mday], $mydate[year]";
 echo "<br/>";
 echo "Staff ID: ".$staffID; 
 echo "<br/>";
-echo "Teaching: ".$grade;
+echo "Teaching: ".$grades;
 echo "<br/>";
 echo "<br/>";
 ?>
 
 
-<?php
-$grade = $_POST['grade'];
-?>
 
 <!-- STUDENTS FROM OTHER CLASSES  -->
 <?php
 $mysqli = new mysqli('localhost','root','','students');
-$grade = $_POST['grade'];
+/* $grade = $_POST['grade']; */
 
 /* $result = $mysqli->query("SELECT * FROM names WHERE grade='$grade' ORDER BY grade, firstName ASC"); */
 $result = $mysqli->query("SELECT * FROM names ORDER BY grade, firstName ASC"); 
+
 ?>
 
 <form name="lunch-form" id="lunch-form" method="POST" onsubmit="populateTable(); return false;"> 
@@ -43,19 +46,23 @@ while($rows = $result->fetch_assoc())
     $grade = $rows['grade'];
     echo "<option id='allStudents' value='$first_name $last_name~$grade'> $first_name $last_name - $grade</option>"; 
 }
+
 ?>
 </select>
 
 <!-- STUDENTS FROM CHOSEN CLASS  -->
 <?php
+
 $mysqli = new mysqli('localhost','root','','students');
 $resultSet = $mysqli->query("SELECT firstName, lastName, grade FROM names");
-$grade = $_POST['grade'];
-$result = $mysqli->query("SELECT * FROM names WHERE grade='$grade' order by firstName ASC ");
+/* $grade = $_POST['grade']; */
+
+
+$result = $mysqli->query("SELECT * FROM names WHERE grade='$grades' order by firstName ASC ");
 
 ?>
 <select name="students" id="students">
-<option value='' selected='selected'><?php echo'--'.$grade,' Students--'?></option>;
+<option value='' selected='selected'><?php echo'--'.$grades,' Students--'?></option>;
 <?php
 while($rows = $result->fetch_assoc())
 {
@@ -71,13 +78,15 @@ while($rows = $result->fetch_assoc())
 
 <br>
 <br>
+
 <button type="submit" name="submit" id="submitBtn">Submit Test</button>
 </form>
 
 <!--AJAX-->
 
 <?php
-require('menus.php')
+require('menus.php');
+require('orderTickets.php');
 ?>
 
  <script>
@@ -86,7 +95,7 @@ require('menus.php')
             e.preventDefault();
             $.ajax( {
                 url: "ordersDB.php",
-                method: "post",
+                method: "POST",
                 data: $("form").serialize(),
                 dataType: "text",
                 success: function() {
