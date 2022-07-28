86cc56c1c3489ebfa88034f8aa6461830114fb64
cDev-Creator <mccoyng170@gmail.com>
2022-07-27 21:38:43 -0400

    different home pages based on user account type

---
+++ officeStaffAccess.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="scss/main.css">
+</head>
+<body>
+
+
+<div class="container">
+<div class="options">
+    <button id="orderLunchBtn"><h2>Order Lunch</a></h2></body>
+    <button><h2><a href="AddNewStudents/addNew.php">New Student</a></h2></button>
+    <button><h2><a href="orderTickets.php">Order Tickets</a></h2></button>
+    <button><h2><a href="SchoolOrderTables/schoolOrders.php">School Orders</a></h2></button> 
+    
+   <button><a href="logout.php">Log Out</a></button>
+ 
+</div>
+</div>
+
+    <div class="modal-bg">
+            <!-- <form method='POST' action="OrderLunch/orderLunch.php" id="form" class="modal-content"> -->
+        <form method='POST' action="OrderLunch/orderLunch.php" id="form" class="modal-content">
+
+            <div class="closeBtn" id="closeBtn">+</div>
+        
+            <div>
+                <label for="grade">Class:</label>
+                <select name="grade" id="grade" required>
+                    <option value="" selected="selected">--select--</option>  
+                    <option value="Office Staff">Office Staff</option>  
+                    <option value="02 Day Preschool">2 Day Preschool</option>
+                    <option value="03 Day Preschool">3 Day Preschool</option>
+                    <option value="1st Grade">1st Grade</option> 
+                    <option value="2nd Grade">2nd Grade</option>  
+                    <option value="3rd Grade">3rd Grade</option>  
+                    <option value="4th Grade">4th Grade</option>  
+                    <option value="5th Grade">5th Grade</option>  
+                    <option value="6th Grade">6th Grade</option>  
+                    <option value="7th Grade">7th Grade</option>  
+                    <option value="8th Grade">8th Grade</option>  
+                </select>
+            </div>
+
+            <div>
+                <button type="submit" id="submitbtn" name="submit">Ok</button>
+            </div>
+
+        </form>
+    </div>  
+  
+</body>
+</html>
+<script src="HomePage/modal.js"></script>   
