95b5cc55bf935281d4d4303d49589ea84a687ab7
cDev-Creator <mccoyng170@gmail.com>
2022-07-31 19:06:13 -0400

    home page in same folder / pages only accessable with valid login

--- HomePage/modal.js
+++ HomePage/modal.js
@@ -1,4 +1,3 @@
-
 document.getElementById('orderLunchBtn').addEventListener('click', 
 function() {
     document.querySelector('.modal-bg').style.display = 'flex';
@@ -9,15 +8,3 @@ document.getElementById('closeBtn').addEventListener('click',
 function() {
     document.querySelector('.modal-bg').style.display = 'none';
 });
-
-
-const staffID = document.getElementById('staffID')
-document.getElementById('form').addEventListener('submit', e() =>{
-    e.preventDefault();
-
-    const request = new XMLHttpRequest();
-    request.open('post', 'OrderLunch/orderLunch.php');
-    request.onload = () => {
-        console.log(request.responseText)
-    }
-})
---
+++ HomePage/officeStaffAccess.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+if(isset($_GET['p'])){
+    $p = $_GET['p'];
+}
+if(!isset($_SESSION['user'])){
+   header("Location:../index.php");
+}
+?>
+<script src="https://kit.fontawesome.com/fa7c02709f.js" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+
+
+<div class="container">
+<div class="options">
+    <button id="orderLunchBtn" class="mainBtns"><h2>Order Lunch</a></h2></body>
+    <button class="mainBtns"><h2><a href="../SchoolOrderTables/schoolOrders.php">School Orders</a></h2></button> 
+    <button class="mainBtns"><h2><a href="../orderTickets.php">Lunch Tickets</a></h2></button>
+    <button class="mainBtns"><h2><a href="../AddNewStudents/addNew.php">Students / Staff</a></h2></button>
+    <button class="logout"><h2><a href="../OrderLunch/logout.php">
+    <i class="fa-solid fa-right-from-bracket fa-xl"></i>
+    </a><h2>
+</button>
+ 
+</div>
+</div>
+
+    <div class="modal-bg">
+            <!-- <form method='POST' action="OrderLunch/orderLunch.php" id="form" class="modal-content"> -->
+        <form method='POST' action="../OrderLunch/orderLunch.php?p=<?php echo $p?>" id="form" class="modal-content">
+
+            <div class="closeBtn" id="closeBtn">+</div>
+        
+            <div>
+    
+                <select name="grade" id="grade" class="dropdown" required>
+                    <option value="" selected="selected">--Select Class--</option>  
+                    <option value="Office Staff">Office Staff</option>  
+                    <option value="02 Day Preschool">2 Day Preschool</option>
+                    <option value="03 Day Preschool">3 Day Preschool</option>
+                    <option value="1st Grade">1st Grade</option> 
+                    <option value="2nd Grade">2nd Grade</option>  
+                    <option value="3rd Grade">3rd Grade</option>  
+                    <option value="4th Grade">4th Grade</option>  
+                    <option value="5th Grade">5th Grade</option>  
+                    <option value="6th Grade">6th Grade</option>  
+                    <option value="7th Grade">7th Grade</option>  
+                    <option value="8th Grade">8th Grade</option>  
+                </select>
+            </div>
+
+            <div>
+                <button type="submit" id="submitGradeBtn" name="submit">Submit</button>
+            </div>
+
+        </form>
+    </div>  
+  
+</body>
+</html>
+<script src="modal.js"></script>   
