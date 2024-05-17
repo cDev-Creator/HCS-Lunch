77a0aa154b9fbd5cc9dc2de9d3b94e07e85f575c
cDev-Creator <mccoyng170@gmail.com>
2024-05-16 19:09:45 -0500

    restaurant name db added, populates tabs with the added names

--- UpdateMenus/menuRotation.php
+++ UpdateMenus/menuRotation.php
@@ -3,6 +3,28 @@ error_reporting(0);
 session_start();
 include("../conn.php");
 $restaurant = $_SESSION['restaurant'];
+
+$restaurantNamesTable = $conn->query("SELECT ID, restaurantName FROM restaurants ORDER BY ID"); 
+if ($restaurantNamesTable->num_rows > 0) {
+    while ($row = $restaurantNamesTable->fetch_assoc()) {
+        $restaurantNames[] = $row['restaurantName'];
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // see if a restaurant button is clicked
+    foreach ($restaurantNames as $index => $restaurantName) {
+        if (isset($_POST['restaurant' . ($index + 1)])) {
+            $activeTabValue = $restaurantName; 
+            break; 
+        }
+
+    }
+}
+
+if (empty($activeTabValue) && !empty($restaurantNames)) {
+    $activeTabValue = $restaurantNames[0];
+}
 ?>
 
 <script src="https://kit.fontawesome.com/fa7c02709f.js" crossorigin="anonymous"></script>
@@ -18,15 +40,20 @@ $restaurant = $_SESSION['restaurant'];
         <button id="addNewMenuItemBtn"><a href='createMenuItem.php'>New Menu Item</a></button>
 </div>
 
-<form action="" method="post" class="tabs">
-	<input type="submit" class="tab active" name="restaurant1" value="54 Pizza">
-	<input type="submit" class="tab" id="rat" name="restaurant2" value="Chick-fil-A">
-	<input type="submit" class="tab" name="restaurant3" value="Ritzy's">
-	<input type="submit" class="tab" name="restaurant4" value="Arby's">
-	<input type="submit" class="tab" name="restaurant5" value="Great Harvest">
+<form action="updateMenu.php" method="post" class="tabs">
+    <?php
+    // loop through restaurant names to generate submit buttons
+    for ($i = 0; $i < count($restaurantNames) && $i < 5; $i++) {
+        $isActive = ($restaurantNames[$i] === $activeTabValue) ? 'active' : '';
+        echo '<input type="submit" class="tab ' . $isActive . '" name="restaurant' . ($i + 1) . '" value="' . $restaurantNames[$i] . '">';
+        echo '<input type="hidden" name="ID"  value="' . $rows['ID'] . '">';
+        echo '<button type="submit" name="update" id="updateRestaurantBtn"><i class="fa-solid fa-pencil fa-lg"></i></button>';
+    }
+    ?>
 </form>
 
 <?php
+
 if (isset($_POST['restaurant1'])) {
 	$restaurant = '54pizza';
 	$_SESSION['restaurant'] = $restaurant;
@@ -52,15 +79,16 @@ else if (isset($_POST['restaurant5'])) {
 	$_SESSION['restaurant'] = $restaurant;
 }
 
-else if($restaurant === null ){
+else if($restaurant === null){
 	$restaurant = '54pizza';
     $_SESSION['restaurant'] = '54pizza';
 }
+echo $activeRestaurant
 ?>
 
 <table id="menuItemsTable" align="center"> 
 	<tr> 
-		<th colspan="7" ><h2 id="menusTitle" >Menu Items</h2></th> 
+		<th colspan="7" ><h2 id="menusTitle" >Menu Items <?php echo $activeTabValue ?></h2></th> 
 		</tr>
             <th> Item </th> 
             <th> Price </th> 
@@ -75,7 +103,6 @@ else if($restaurant === null ){
 		<?php while($rows = $result->fetch_assoc())
 		{ 
 		?> 
-	
         <form action="updateMenu.php" method="post">
             <td name='item'><?php echo $rows['item']; ?></td>
             <td name='price'><?php echo $rows['price'];?>
@@ -98,12 +125,19 @@ else if($restaurant === null ){
 				</button> 
 				</form>
 		    </td>
-		</tr>  
+		</tr>
+		
 	<?php 
-    
         } 
     ?> 
 	</table> 
+	<?php 
+
+	// array of restaurant names, remove
+	echo "<pre>";
+	print_r($restaurantNames);
+	echo "</pre>";
+	?> 
+
 </body> 
 </html>
-
