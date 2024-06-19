f6455574092ac6187ea9019f47bda0f0e3d79483
cDev-Creator <mccoyng170@gmail.com>
2024-06-19 14:35:00 -0500

    removed text banner when creating new menu item

--- UpdateMenus/createMenuItem.php
+++ UpdateMenus/createMenuItem.php
@@ -1,27 +1,3 @@
-<?php
-session_start();
-$restaurant = $_SESSION['restaurant'];
-$restaurantText;
-
-if($restaurant === "54pizza") {
-    $restaurantText = "54 Pizza";
-}
-else if($restaurant === "chickfila") {
-    $restaurantText = "Chick-fil-A";
-}
-else if($restaurant === "ritzys") {
-    $restaurantText = "Ritzy's";
-}
-else if($restaurant === "arbys") {
-    $restaurantText = "Arby's";
-}
-else if($restaurant === "greatharvest") {
-    $restaurantText = "Great Harvest";
-}
-echo "<h2 id='addNewMsg'>Add a new menu item to {$restaurantText}</h2>";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
